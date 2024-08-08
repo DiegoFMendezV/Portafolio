acd66e17933934fd36a29639b89707b36283b77e
DiegoFMendezV <difemeva@hotmail.com>
2024-08-08 12:20:52 -0500

    Inicio responsive

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -6,11 +6,11 @@ export const Inicio = () => {
   return (
     <section className='wrapContent bg1'>
         <div className='heroContent'>
-            <div>
+            <div className='info1'>
                 <p>Yo soy</p>
                 <h1 className='background'>DESARROLLADOR <br />WEB <br />MULTIMEDIA <span className='point'></span></h1>
             </div>
-            <div className='info'>
+            <div className='info2'>
                 <p>Bienvenid@ a mi Portafolio</p>
                 <button className='btnCV'>
                     <span>Descarga mi CV</span>
