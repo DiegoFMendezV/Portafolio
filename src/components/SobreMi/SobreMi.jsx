e24b109099ea56b1bc12f263f16b691675d4f105
DiegoFMendezV <difemeva@hotmail.com>
2024-09-09 17:42:52 -0500

    ajustes foto sobremi

--- src/components/SobreMi/SobreMi.jsx
+++ src/components/SobreMi/SobreMi.jsx
@@ -1,3 +1,4 @@
+import foto from '../../assets.foto.jpg'
 import '../../components/SobreMi/SobreMi.css'
 
 function SobreMi () {
@@ -6,7 +7,7 @@ function SobreMi () {
       <article className="wrapContent bg1">
         <div className="subWrap">
           <div className="image">
-            <img src="src/assets/FOTO.jpg" alt="" />
+            <img src={foto} alt="" />
           </div>
           <div className="contentSobreMi">
             <h3>SOBRE MI</h3>
